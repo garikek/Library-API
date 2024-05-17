7bd0a8ae3f5dd973ecc82d24740be9b840de913b
garikek <yan.buinov@gmail.com>
2024-05-17 21:00:39 +0300

    add authentication via bearer token

---
+++ AuthenticationService/src/main/java/com/example/AuthenticationService/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.AuthenticationService.repository;
+
+import com.example.AuthenticationService.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    public User findUserByEmail(String email){
+        User user = new User(email,"123456");
+        user.setFirstName("FirstName");
+        user.setLastName("LastName");
+        return user;
+    }
+}
